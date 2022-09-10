04e8b388658d9533cb7d9c3ff608af7eeefe8e76
SediAB <abbasnasabs@gmail.com>
2022-09-10 15:11:29 +1200

    Added my intermediate SQL codes

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -19,7 +19,10 @@ INSERT INTO EmployeeDemographics VALUES
 (1006, 'Michael', 'Scott', 35, 'Male'),
 (1007, 'Meredith', 'Palmer', 32, 'Female'),
 (1008, 'Stanley', 'Hudson', 38, 'Male'),
-(1009, 'Kevin', 'Malone', 31, 'Male')
+(1009, 'Kevin', 'Malone', 31, 'Male'),
+(1010, 'Ryan' , 'Howard', 26, 'Male'),
+(1011, 'Holly' , 'Flax' , 23, 'NULL'),
+(1012, 'Darryl' , 'Philbin' , 30, 'Male')
 
 Insert Into EmployeeSalary VALUES
 (1001, 'Salesman', 45000),
@@ -30,7 +33,10 @@ Insert Into EmployeeSalary VALUES
 (1006, 'Regional Manager', 65000),
 (1007, 'Supplier Relations', 41000),
 (1008, 'Salesman', 48000),
-(1009, 'Accountant', 42000)
+(1009, 'Accountant', 42000),
+(1010, 'NULL', 47000),
+(1011, 'Salesman', 43000)
+
 
 /*
 Select Statement
@@ -128,4 +134,235 @@ FROM EmployeeDemographics
 WHERE Age > 31
 GROUP BY Gender
 ORDER BY Gender DESC
+
+
+/*
+Inner Joins, Full/Left/Right/Outer Joins
+*/
+
+SELECT *
+FROM [SQL Practice].dbo.EmployeeDemographics
+Inner Join [SQL Practice].dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+SELECT *
+FROM [SQL Practice].dbo.EmployeeDemographics
+Full Outer Join [SQL Practice].dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+SELECT EmployeeSalary.EmployeeID, FirstName, LastName, JobTitle, Salary
+FROM [SQL Practice].dbo.EmployeeDemographics
+Right Outer Join [SQL Practice].dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+SELECT EmployeeDemographics.EmployeeID, FirstName, LastName, JobTitle, Salary
+FROM [SQL Practice].dbo.EmployeeDemographics
+Right Outer Join [SQL Practice].dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EMployeeSalary.EmployeeID
+
+SELECT EmployeeDemographics.EmployeeID, FirstName, LastName, JobTitle, Salary
+FROM [SQL Practice].dbo.EmployeeDemographics
+Left Outer Join [SQL Practice].dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EMployeeSalary.EmployeeID
+
+SELECT EmployeeDemographics.EmployeeID, FirstName, LastName, Salary
+FROM [SQL Practice].dbo.EmployeeDemographics
+Inner Join [SQL Practice].dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+WHERE FirstName <> 'Michael'
+ORDER BY Salary DESC
+
+SELECT [EmployeeID],
+[FirstName],
+[LastName],
+[Age],
+[Gender],
+COUNT(*) AS CNT
+FROM [SQL Practice].dbo.EmployeeDemographics
+GROUP BY EmployeeID,
+FirstName,
+LastName,
+Age,
+Gender
+HAVING COUNT(*) > 1;
+
+SELECT [EmployeeID],
+[JobTitle],
+[Salary],
+COUNT(*) AS CNT
+FROM [SQL Practice].dbo.EmployeeSalary
+GROUP BY EmployeeID,
+JobTitle,
+Salary
+HAVING COUNT(*) > 1;
+
+WITH CTE ([EmployeeID],
+[FirstName],
+[LasetName],
+[Age],
+[Gender],
+duplicatecount)
+AS (SELECT [EmployeeID],
+[FirstName],
+[LastName],
+[Age],
+[Gender],
+Row_Number() OVER (PARTITION BY [EmployeeID],
+[FirstName],
+[LastName],
+[Age],
+[Gender]
+ORDER BY EmployeeID) AS DuplicateCount
+FROM [SQL Practice].dbo.EmployeeDemographics)
+DELETE FROM CTE
+WHERE duplicateCount > 1;
+
+WITH CTE ([EmployeeID],
+[JobTitle],
+[Salary],
+duplicatecount)
+AS (SELECT [EmployeeID],
+[JobTitle],
+[Salary],
+Row_Number() OVER (PARTITION BY [EmployeeID],
+[JobTitle],
+[Salary]
+ORDER BY EmployeeID) AS DuplicateCount
+FROM [SQL Practice].dbo.EmployeeSalary)
+DELETE FROM CTE
+WHERE duplicateCount > 1;
+
+SELECT JobTitle, AVG(Salary)
+FROM [SQL Practice].dbo.EmployeeDemographics
+Inner Join [SQL Practice].dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+WHERE JobTitle = 'Salesman'
+GROUP BY JobTitle
+
+/*
+Union, Union All
+*/
+
+
+CREATE TABLE WarehouseEmployeeDemographics
+(EmployeeID int,
+FirstName varchar(50),
+LastName varchar(50),
+Age int,
+Gender varchar(50)
+)
+
+INSERT INTO WarehouseEmployeeDemographics VALUES
+(1050, 'Roy', 'Anderson', 31, 'Male'),
+(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+(1052, 'Val', 'Johnson', 31, 'Female'),
+(1013, 'Darryl', 'Philbin', NULL, 'Male')
+ 
+
+SELECT *
+FROM WarehouseEmployeeDemographics
+
+SELECT *
+FROM [SQL Practice].dbo.EmployeeDemographics
+UNION ALL
+SELECT *
+FROM [SQL Practice].dbo.WarehouseEmployeeDemographics
+
+SELECT *
+FROM [SQL Practice].dbo.EmployeeDemographics
+UNION
+SELECT *
+FROM [SQL Practice].dbo.WarehouseEmployeeDemographics
+ORDER BY EmployeeID
+
+/*
+Case Statement
+*/
+
+SELECT FirstName, LastName, Age,
+CASE
+WHEN Age > 30 THEN 'Old'
+WHEN Age BETWEEN 27 AND 30 THEN 'Young'
+ELSE 'Baby'
+END
+FROM [SQL Practice].dbo.EmployeeDemographics
+WHERE Age is NOT NULL
+ORDER BY Age
+
+SELECT FirstName, LastName, JobTitle, Salary,
+CASE
+WHEN JobTitle = 'Salesman' THEN Salary + (Salary * .10)
+WHEN JobTitle = 'Accountant' THEN Salary + (Salary * .05)
+WHEN JobTitle = 'HR' THEN Salary + (Salary * .000001)
+ELSE Salary + (Salary * .03)
+END AS SalaryAfterRaise
+FROM [SQL Practice].dbo. EmployeeDemographics
+JOIN [SQL Practice].dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+/*
+Having Clause
+*/
+
+SELECT JobTitle, AVG(Salary)
+FROM [SQL Practice].dbo.EmployeeDemographics
+JOIN [SQL Practice].dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING AVG(Salary) > 45000
+ORDER BY AVG (Salary)
+
+/*
+Updating/Deleting Data
+*/
+
+SELECT * 
+FROM [SQL Practice].dbo.EmployeeSalary
+
+UPDATE [SQL Practice].dbo.EmployeeSalary
+SET JobTitle = 'HR'
+WHERE EmployeeID = 1010
+
+UPDATE [SQL Practice].dbo.EmployeeSalary
+SET Salary = 55000
+WHERE JobTitle = 'Regional Manager'
+
+DELETE
+FROM [SQL Practice].dbo.EmployeeDemographics
+WHERE EmployeeID = 1005
+
+/*
+Aliasing
+*/
+
+SELECT FirstName + ' ' + LastName AS FullName
+FROM [SQL Practice].dbo.EmployeeDemographics
+
+SELECT Avg(Age) AS AvgAge
+FROM [SQL Practice].dbo.EmployeeDemographics
+
+SELECT Demo.EmployeeID, Sal.Salary
+FROM [SQL Practice].[dbo].[EmployeeDemographics] AS Demo
+JOIN [SQL Practice].[dbo].[EmployeeSalary] AS Sal
+ON Demo.EmployeeID = Sal.EmployeeID
+
+SELECT Demo.EmployeeID, Demo.FirstName, Sal.JobTitle
+FROM [SQL Practice].dbo.EmployeeDemographics Demo
+LEFT JOIN [SQL Practice].dbo.EmployeeSalary Sal
+ON Demo.EmployeeID = Sal.EmployeeID
+
+/*
+Partition By
+*/
+
+SELECT FirstName, LastName, Gender, Salary
+, COUNT(Gender) OVER (PARTITION BY Gender) as TotalGender
+FROM [SQL Practice].dbo.EmployeeDemographics dem
+JOIN [SQL Practice].dbo.EmployeeSalary sal
+ON dem.EmployeeID = sal.EmployeeID
+
+
+
+
+
 
